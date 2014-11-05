93956aacd41aa580d8a108802009b7396423e7ff
Sergio Correia <sergio.correia@gmail.com>
2014-11-05 02:12:32 -0500

    bf

--- StataEditorPlugin.py
+++ StataEditorPlugin.py
@@ -6,7 +6,7 @@ import sublime, sublime_plugin
 try:
 	import Pywin32.setup, win32com.client, win32con, win32api
 except:
-	sublime.status_message("Cannot interact with Stata - Install the Pywin32 package!!!!")
+	sublime.status_message("StataEditor - Step 2: Install the Pywin32 package")
 	raise Exception
 
 import os, tempfile, subprocess, re, urllib, json, random, time, calendar, winreg
@@ -222,7 +222,7 @@ class StataUpdateExecutablePath(sublime_plugin.ApplicationCommand):
 			pass
 
 		fn = get_exe_path()
-		msg ="Enter the path of the Stata executable"
+		msg ="StataEditor - Step 3: Enter the path of the Stata executable"
 		sublime.active_window().show_input_panel(msg, fn, update_settings, check_correct, cancel_update)
 
 
@@ -415,7 +415,7 @@ def launch_stata():
 		win32api.WinExec(stata_fn, win32con.SW_SHOWMINNOACTIVE)
 		sublime.stata = win32com.client.Dispatch ("stata.StataOLEApp")
 	except:
-		sublime.error_message("Cannot run Stata; register Stata Automation type library: {}".format(stata_fn))
+		sublime.error_message("StataEditor - Step 4: Register the Stata Automation type library, see http://www.stata.com/automation/#install")
 
 	# Stata takes a while to start and will silently discard commands sent until it finishes starting
 	# Workaround: call a trivial command and see if it was executed (-local- in this case)
